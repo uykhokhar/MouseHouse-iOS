5feef4df260ab31a7a092320fb1fea70c4feb508
Daniel Brajkovic <daniel@brajkovic.com>
2012-04-06 00:52:48 -0500

    Labels and cards being drawn to scroll view

--- MouseHouse-iOS/MHCagesViewController.m
+++ MouseHouse-iOS/MHCagesViewController.m
@@ -41,9 +41,33 @@
 {
     // Update the user interface for the detail item.
 
-    if (self.rack) {
+    //if (self.rack) {
+        NSArray *columnHeaders = [@"A B C D E F G H I J K L M N O P" componentsSeparatedByString:@" "];
         [_cagesScrollView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:NULL];
-    }
+        int cols, rows;
+        for (cols = 0; cols < 10; cols++) {
+            UILabel *columnHeader = [[UILabel alloc] initWithFrame:CGRectMake(cols*236, 0, 236, _rackColumnHeaderScrollView.bounds.size.height)];
+            [columnHeader setTextAlignment:UITextAlignmentCenter];
+            [columnHeader setText:[columnHeaders objectAtIndex:cols]];
+            [_rackColumnHeaderScrollView addSubview:columnHeader];
+            for (rows = 0; rows < 7; rows++) {
+                if (cols == 0) {
+                    UILabel *rowHeader = [[UILabel alloc] initWithFrame:CGRectMake(0, rows*107, _rackRowHeaderScrollView.bounds.size.width, 107)];
+                    [rowHeader setTextAlignment:UITextAlignmentCenter];
+                    [rowHeader setText:[NSString stringWithFormat:@"%d", rows +1]];
+                    [_rackRowHeaderScrollView addSubview:rowHeader];
+                }
+                UIView *cardView = [[UIView alloc] initWithFrame:CGRectMake(cols*236, rows*107, 236, 107)];
+                UIImageView *cardImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"mockup_cage_card"]];
+                [cardImage setContentMode:UIViewContentModeScaleAspectFit];
+                cardImage.frame = CGRectMake(2, 2, 232, 103);
+                [cardView addSubview:cardImage];
+                [_cagesScrollView addSubview:cardView];
+            }
+        }
+        [_cagesScrollView setContentSize:CGSizeMake(cols*236, rows*107)];
+        [_rackColumnHeaderScrollView setContentSize:CGSizeMake(cols*236, rows*107)];
+    //}
 }
 
 - (void)viewDidLoad
